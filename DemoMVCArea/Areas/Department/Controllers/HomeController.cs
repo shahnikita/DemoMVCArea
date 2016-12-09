4139f3050f77e5039e69cd445134c18849bd21c8
Nikita Shah <nikitashah@rangamindia.com>
2016-12-09 05:25:32 -0500

    Added my project

---
+++ DemoMVCArea/Areas/Department/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMVCArea.Areas.Department.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Department/Departement/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
